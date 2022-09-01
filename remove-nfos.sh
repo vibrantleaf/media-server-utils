fa6074f267137859bf6ca2c6a5c6b68f1c6ac7df
julia <bigowo.digital@outlook.com>
2022-09-01 08:28:08 -0700

    Create remove-nfos.sh

---
+++ remove-nfos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# MEDIA_PATH = /path/to/your/media/ {Anime, Shows, Movies , etc}
+MEDIA_PATH=""
+
+
+# remove all .nfo(s) from all episodes from all seasons from all shows & movies & music
+rm $MEDIA_PATH/{*.nfo,*/*.nfo,*/*/*.nfo,*/*/*.nfo,*/*/*/*.nfo -v
